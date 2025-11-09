83c88a20064bef79c57b48d0ec40f6656534a1e6
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-11-09 11:34:39 +0900

    [SPR-6] 기본 기능 구현 완료

---
+++ src/main/java/SpringSprout/BeanFactory.java
@@ -0,0 +1,12 @@
+package SpringSprout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class BeanFactory {
+
+    HashMap<String, Object> beans = new HashMap<>();
+}
