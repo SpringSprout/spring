a9ea3f713df9cd847e57da302925d1e901a05f14
lhoju0158 <lhoju0158@gmail.com>
2025-11-18 14:19:51 +0900

    [SPR-8] feat: ClassScanner 구현

--- sprout/core/core-impl/src/main/java/com/spring/sprout/context/ResourcePatternResolver.java
+++ sprout/core/core-impl/src/main/java/com/spring/sprout/context/ResourcePatternResolver.java
@@ -1,4 +1,14 @@
 package com.spring.sprout.context;
 
+import java.io.IOException;
+
 public interface ResourcePatternResolver {
+
+    String CLASSPATH_URL_PREFIX = "classpath:";
+
+    Resource[] getResources(String locationPattern) throws IOException;
+
+    Resource getResource(String location);
+
+    ClassLoader getClassLoader();
 }
--- sprout/global/src/main/java/com/spring/sprout/error/ErrorMessage.java
+++ sprout/global/src/main/java/com/spring/sprout/error/ErrorMessage.java
@@ -7,7 +7,10 @@ public enum ErrorMessage {
     NO_UNIQUE_BEAN_FOUND_WITH_TYPE("해당 타입을 가진 빈은 2개 이상 존재합니다."),
 
     // 설정 파일 오류
-    FILE_NOT_LOADED("설정 파일이 로드되지 않았습니다.");
+    FILE_NOT_LOADED("설정 파일이 로드되지 않았습니다."),
+
+    FILE_NOT_FOUND("해당 경로에 파일이 없습니다."),
+    FILE_NOT_RESOLVED("해당 파일를 로드할 수 없습니다.");
 
     private final String message;
 
